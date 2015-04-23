8fcaee86a4e8208779c7d3e8c58d3ab8a33459d2
kolya19951 <kolya.simotyuk@gmail.com>
2015-04-23 20:32:21 +0300

    Update Prime_triplets.java

--- src/Prime_triplets.java
+++ src/Prime_triplets.java
@@ -1,8 +1,15 @@
 /**
- * Created by Денис on 21.04.2015.
+ * Created by пїЅпїЅпїЅпїЅпїЅ on 21.04.2015.
  */
 public class Prime_triplets {
     public static void main(String[] args) {
-
+        long line = 10000, result = 0;
+        ShiftingTable table = new ShiftingTable(line);
+        for (int i = 0; i <= line ; i++) {
+            if (table.IsInTriplet())
+                result += table.GetData(0, 0);
+            table.ShiftInPyramid();
+        }
+        System.out.println(result);
     }
 }
